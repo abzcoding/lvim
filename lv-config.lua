6cb031d40b30ad2d13dfc14cc6773a969c8198b0
abzcoding <abzcoding@gmail.com>
2021-07-29 21:40:21 +0430

    where did it go?

--- lv-config.lua
+++ lv-config.lua
@@ -70,11 +70,52 @@ lvim.builtin.terminal.execs = {
 lvim.lsp.override = { "rust" }
 lvim.lang.go.formatter.exe = "goimports"
 lvim.lang.python.formatter.exe = "yapf"
-
+-- vim.g.compiler_gcc_ignore_unmatched_lines = true
+-- vim.g.compiler_clang_ignore_unmatched_lines = true
+-- quickfix_entries = {}
+-- function on_stdout(_, data, _)
+-- 	for _, s in ipairs(data) do
+-- 		local vals = {}
+-- 		for str in string.gmatch(s, "([^:]+)") do
+-- 			table.insert(vals, str)
+-- 		end
+-- 		table.insert(quickfix_entries, s)
+-- 	end
+-- end
+--
+-- -- local efm
+-- function on_exit(on_build_done)
+-- 	local efm = '%f:%l:%c: %t%s: %m,%-G%.%#,'-- .. vim.api.nvim_get_option("errorformat") .. ""
+-- 	vim.fn.setqflist({}, " ", {
+-- 		lines = quickfix_entries,
+-- 		efm = efm,
+-- 	})
+-- 	local qfs = vim.fn.getqflist()
+-- 	if #qfs > 0 then
+-- 		vim.cmd("copen")
+-- 		-- vim.cmd("cnext")
+-- 	end
+--
+-- 	if on_build_done then
+-- 		local found_error = false
+-- 		for _, s in ipairs(qfs) do
+-- 			if s.type == "e" then
+-- 				found_error = true
+-- 				break
+-- 			end
+-- 		end
+-- 		on_build_done(found_error)
+-- 	end
+-- end
+--
+-- on_build_done = function(failure)
+-- 	-- vim.cmd[[cclose]]
+-- end
 -- Autocommands
 lvim.autocommands.custom_groups = {
   -- c, cpp
   { "Filetype", "c,cpp", "nnoremap <leader>m <cmd>lua require('core.terminal')._exec_toggle('make;read')<CR>"},
+  -- { "Filetype", "c,cpp", "nnoremap <leader>m <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='make;read', hidden =false, on_stdout = on_stdout, on_stderr = on_stdout, on_exit = function() on_exit(on_build_done) end}:toggle()<CR>"},
   { "Filetype", "c,cpp", "nnoremap <leader>r <cmd>lua require('core.terminal')._exec_toggle('make run;read')<CR>"},
   { "Filetype", "c,cpp", "nnoremap <leader>t <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='make test;read', hidden =false}:toggle()<CR>"},
   { "Filetype", "cpp", "nnoremap <leader>H <Cmd>ClangdSwitchSourceHeader<CR>" },
