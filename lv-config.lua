87a1f5d4a381f166ad6dcaf5971752c1801d0213
abzcoding <abzcoding@gmail.com>
2021-07-29 20:41:59 +0430

    you might want to toggle the result of tests

--- lv-config.lua
+++ lv-config.lua
@@ -75,14 +75,14 @@ lvim.lang.python.formatter.exe = "yapf"
 lvim.autocommands.custom_groups = {
   -- c, cpp
   { "Filetype", "c,cpp", "nnoremap <leader>m <cmd>lua require('core.terminal')._exec_toggle('make;read')<CR>"},
-  { "Filetype", "c,cpp", "nnoremap <leader>r <cmd>lua require('core.terminal')._exec_toggle('make run;read')<CR>"},
-  { "Filetype", "c,cpp", "nnoremap <leader>t <cmd>lua require('core.terminal')._exec_toggle('make test;read')<CR>"},
-  { "Filetype", "c,cpp", "nnoremap <leader>H <Cmd>ClangdSwitchSourceHeader<CR>" },
+  { "Filetype", "c,cpp", "nnoremap <leader>t <cmd>lua require('core.terminal')._exec_toggle('make run;read')<CR>"},
+  { "Filetype", "c,cpp", "nnoremap <leader>r <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='make test;read', hidden =false}:toggle()<CR>"},
+  { "Filetype", "cpp", "nnoremap <leader>H <Cmd>ClangdSwitchSourceHeader<CR>" },
 
   -- rust
-  { "Filetype", "rust", "nnoremap <leader>r <cmd>lua require('core.terminal')._exec_toggle('cargo build;read')<CR>"},
+  { "Filetype", "rust", "nnoremap <leader>m <cmd>lua require('core.terminal')._exec_toggle('cargo build;read')<CR>"},
   { "Filetype", "rust", "nnoremap <leader>r <cmd>lua require('core.terminal')._exec_toggle('cargo run;read')<CR>"},
-  { "Filetype", "rust", "nnoremap <leader>t <cmd>lua require('core.terminal')._exec_toggle('cargo test -- --ignored;read')<CR>"},
+  { "Filetype", "rust", "nnoremap <leader>t <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='cargo test -- --ignored;read', hidden =false}:toggle()<CR>"},
   { "Filetype", "rust", "nnoremap <leader>H <cmd>lua require('core.terminal')._exec_toggle('cargo clippy;read')<CR>"},
   { "Filetype", "rust", "nnoremap <leader>lm <Cmd>RustExpandMacro<CR>" },
   { "Filetype", "rust", "nnoremap <leader>lH <Cmd>RustToggleInlayHints<CR>" },
@@ -91,7 +91,7 @@ lvim.autocommands.custom_groups = {
 
   -- python
   { "Filetype", "python", "nnoremap <leader>r <cmd>lua require('core.terminal')._exec_toggle('python " .. vim.fn.expand("%") .. ";read')<CR>"},
-  { "Filetype", "python", "nnoremap <leader>t <cmd>lua require('core.terminal')._exec_toggle('python -m test -j0;read')<CR>"},
+  { "Filetype", "python", "nnoremap <leader>t <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='python -m test -j0;read', hidden =false}:toggle()<CR>"},
   { "Filetype", "python", "nnoremap <leader>m <cmd>lua require('core.terminal')._exec_toggle('echo \"compile :pepelaugh:\";read')<CR>"},
 }
 
