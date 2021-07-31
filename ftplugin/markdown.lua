8a70e787b279d557044db0b80ccb090e63ff8341
abzcoding <abzcoding@gmail.com>
2021-07-31 14:26:33 +0430

    fix tailwind markdown issues

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,67 +1,13 @@
-lvim.lang.markdown = {
-  formatters = {
-    {
-      exe = "prettier",
-      args = {},
-    },
+lvim.lang.markdown.formatters = {
+  {
+    exe = "prettier",
+    args = {},
   },
-  linters = {
-    {
-      exe = "write_good",
-      args = {},
-    },
-  },
-  lsp = {
-    provider = "tailwindcss",
-    setup = {
-      cmd = {
-        DATA_PATH .. "/lspinstall/tailwindcss/tailwindcss-intellisense.sh",
-        "--stdio",
-      },
-      on_attach = require("lsp").common_on_attach,
-      on_init = require("lsp").common_on_init,
-      capabilities = require("lsp").common_capabilities(),
-      filetypes = { "markdown" },
-      init_options = {
-        userLanguages = {
-          md = "markdown",
-        },
-      },
-      on_new_config = function(new_config)
-        if not new_config.settings then
-          new_config.settings = {}
-        end
-        if not new_config.settings.editor then
-          new_config.settings.editor = {}
-        end
-        if not new_config.settings.editor.tabSize then
-          -- set tab size for hover
-          new_config.settings.editor.tabSize = vim.lsp.util.get_effective_tabstop()
-        end
-      end,
-      root_dir = require("lspconfig").util.root_pattern(
-        "tailwind.config.js",
-        "tailwind.config.ts",
-        "postcss.config.js",
-        "postcss.config.ts",
-        "package.json",
-        "node_modules",
-        ".git"
-      ),
-      settings = {
-        tailwindCSS = {
-          lint = {
-            cssConflict = "warning",
-            invalidApply = "error",
-            invalidConfigPath = "error",
-            invalidScreen = "error",
-            invalidTailwindDirective = "error",
-            invalidVariant = "error",
-            recommendedVariantOrder = "warning",
-          },
-          validate = true,
-        },
-      },
-    },
+}
+
+lvim.lang.markdown.linters = {
+  {
+    exe = "write_good",
+    args = {},
   },
 }
--- lv-config.lua
+++ lv-config.lua
@@ -66,8 +66,9 @@ lvim.builtin.terminal.execs = {
 
 -- Language Specific
 -- =========================================
+lvim.lang.markdown = {}
 lvim.builtin.lspinstall.on_config_done = function ()
-  require("lsp").setup("markdown")
+  require("user.tailwind").setup()
 end
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.override = { "rust", "java" }
