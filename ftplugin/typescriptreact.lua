0a843fe48ebb48b5698da264296eea7247464538
abzcoding <abzcoding@gmail.com>
2021-07-31 20:01:07 +0430

    typescriptreact stuff

---
+++ ftplugin/typescriptreact.lua
@@ -0,0 +1,12 @@
+lvim.lang.typescriptreact.formatters = {
+  {
+    exe = "prettier",
+    args = {},
+  },
+}
+lvim.lang.typescriptreact.linters = {
+  {
+    exe = "eslint_d",
+    args = {},
+  },
+}
