56ad0e8ee5827796dd8ef7c233612ff108dc1cfb
abzcoding <abzcoding@gmail.com>
2021-08-02 13:41:33 +0430

    better indent_line

--- lua/user/indent_blankline.lua
+++ lua/user/indent_blankline.lua
@@ -1,16 +1,47 @@
 local M = {}
 
 M.setup = function()
-  vim.g.indentLine_enabled = 1
-  vim.g.indent_blankline_char = "▏"
+  vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
   vim.g.indent_blankline_filetype_exclude = {
     "help",
-    "terminal",
+    "startify",
     "dashboard",
+    "packer",
+    "neogitstatus",
+    "NvimTree",
+    "Trouble",
   }
-  vim.g.indent_blankline_buftype_exclude = { "terminal" }
+  vim.g.indentLine_enabled = 1
+  -- vim.g.indent_blankline_char = "│"
+  vim.g.indent_blankline_char = "▏"
   vim.g.indent_blankline_show_trailing_blankline_indent = false
   vim.g.indent_blankline_show_first_indent_level = true
+  vim.g.indent_blankline_use_treesitter = true
+  vim.g.indent_blankline_show_current_context = true
+  vim.g.indent_blankline_context_patterns = {
+    "class",
+    "return",
+    "function",
+    "method",
+    "^if",
+    "^while",
+    "jsx_element",
+    "^for",
+    "^object",
+    "^table",
+    "block",
+    "arguments",
+    "if_statement",
+    "else_clause",
+    "jsx_element",
+    "jsx_self_closing_element",
+    "try_statement",
+    "catch_clause",
+    "import_statement",
+    "operation_type",
+  }
+  -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+  vim.wo.colorcolumn = "99999"
 end
 
 return M
