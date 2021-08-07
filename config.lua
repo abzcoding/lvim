a2eef03ee7076cb5de2d2485a669db388b8125f8
abzcoding <abzcoding@gmail.com>
2021-08-08 04:13:29 +0430

    disabled lastplace plugin for now

--- config.lua
+++ config.lua
@@ -37,7 +37,7 @@ lvim.debug = false
 
 -- Known Issues
 -- =========================================
-lvim.builtin.lastplace = { active = true } -- change to false if you are jumping to future
+lvim.builtin.lastplace = { active = false } -- change to false if you are jumping to future
 lvim.builtin.tabnine = { active = true } -- change to false if you don't like tabnine
 lvim.builtin.persistence = { active = true } -- change to false if you don't want persistence
 lvim.builtin.presence = { active = false } -- change to true if you want discord presence
