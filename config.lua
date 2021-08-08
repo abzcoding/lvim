9da19fc080f2da90d13ed07fef416cd6e4b26f37
abzcoding <abzcoding@gmail.com>
2021-08-08 12:58:54 +0430

    cleanup

--- config.lua
+++ config.lua
@@ -30,19 +30,6 @@ lvim.format_on_save = false
 lvim.leader = " "
 lvim.colorscheme = "spacegray"
 lvim.debug = false
--- if you want to choose the source of formatting
--- lvim.lsp.on_init_callback = function(client, _bufnr)
---   client.resolved_capabilities.document_formatting = true
--- end
-
--- Known Issues
--- =========================================
-lvim.builtin.lastplace = { active = false } -- change to false if you are jumping to future
-lvim.builtin.tabnine = { active = true } -- change to false if you don't like tabnine
-lvim.builtin.persistence = { active = true } -- change to false if you don't want persistence
-lvim.builtin.presence = { active = false } -- change to true if you want discord presence
-lvim.builtin.orgmode = { active = false } -- change to true if you want orgmode.nvim
-lvim.builtin.dap.active = false -- change this to enable/disable debugging
 
 -- Default options
 -- =========================================
@@ -54,6 +41,20 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 5
 vim.opt.guifont = "FiraCode Nerd Font:h15"
 vim.opt.cmdheight = 1
+-- if you want to choose the source of formatting
+-- lvim.lsp.on_init_callback = function(client, _bufnr)
+--   client.resolved_capabilities.document_formatting = true
+-- end
+
+-- Customization
+-- =========================================
+lvim.builtin.lastplace = { active = false } -- change to false if you are jumping to future
+lvim.builtin.tabnine = { active = true } -- change to false if you don't like tabnine
+lvim.builtin.persistence = { active = true } -- change to false if you don't want persistence
+lvim.builtin.presence = { active = false } -- change to true if you want discord presence
+lvim.builtin.orgmode = { active = false } -- change to true if you want orgmode.nvim
+lvim.builtin.dap.active = false -- change this to enable/disable debugging
+lvim.builtin.treesitter.indent = { enable = false }
 
 -- Builtin
 -- =========================================
@@ -61,11 +62,6 @@ lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.matchup.enable = true
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.context_commentstring.enable = true
-lvim.builtin.treesitter.indent = { enable = false }
-if lvim.builtin.tabnine.active then
-  lvim.builtin.compe.source.tabnine = { kind = " ", priority = 200, max_reslts = 6 }
-end
--- lvim.builtin.compe.source.orgmode = true
 lvim.builtin.dashboard.active = true
 lvim.builtin.galaxyline.active = true
 lvim.builtin.telescope.defaults.path_display = { shorten = 10 }
@@ -80,6 +76,12 @@ lvim.builtin.dashboard.custom_section["m"] = {
   description = { "  Marks              " },
   command = "Telescope marks",
 }
+if lvim.builtin.tabnine.active then
+  lvim.builtin.compe.source.tabnine = { kind = " ", priority = 200, max_reslts = 6 }
+end
+if lvim.builtin.orgmode.active then
+  lvim.builtin.compe.source.orgmode = true
+end
 -- lvim.builtin.nvimtree.hide_dotfiles = 0
 -- lvim.treesitter.textsubjects.enable = true
 -- lvim.treesitter.playground.enable = true
