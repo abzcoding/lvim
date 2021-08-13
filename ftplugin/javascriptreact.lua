89b4cd3b951cdc60256c43c99ad82a1dfa107e39
abzcoding <abzcoding@gmail.com>
2021-08-13 12:12:50 +0430

    add ftplugin javascriptreact

---
+++ ftplugin/javascriptreact.lua
@@ -0,0 +1,12 @@
+lvim.lang.javascriptreact.formatters = {
+  {
+    exe = "prettier",
+    -- args = {},
+  },
+}
+lvim.lang.javascriptreact.linters = {
+  {
+    exe = "eslint_d",
+    -- args = {},
+  },
+}
