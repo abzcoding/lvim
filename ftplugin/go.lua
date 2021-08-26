aa5bc37ef7bd08a988d3b2e7ca552c6e8f3fc159
Abouzar Parvan <abz@sotoon.ir>
2021-08-26 14:38:52 +0430

    go_delve_dbg seems to be working better

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -8,5 +8,5 @@ lvim.lang.go.formatters = {
 
 if lvim.builtin.dap.active then
   local dap_install = require "dap-install"
-  dap_install.config("go_dbg", {})
+  dap_install.config("go_delve_dbg", {})
 end
