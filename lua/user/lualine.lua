0ea48b65ad0d21b882438422c1b148f7c672cbf4
abzcoding <abzcoding@gmail.com>
2021-09-02 01:36:24 +0430

    removal of nvim-gps

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -48,28 +48,28 @@ local default_colors = {
   git = { change = "#ECBE7B", add = "#98be65", delete = "#ec5f67", conflict = "#bb7a61" },
 }
 
-local function gps_available()
-  local status_ok, gps = pcall(require, "nvim-gps")
-  if not status_ok then
-    return nil
-  end
-  if (gps.is_available() == true) and (vim.fn.winwidth(0) > 120) then
-    return true
-  end
-  return nil
-end
+-- local function gps_available()
+--   local status_ok, gps = pcall(require, "nvim-gps")
+--   if not status_ok then
+--     return nil
+--   end
+--   if (gps.is_available() == true) and (vim.fn.winwidth(0) > 120) then
+--     return true
+--   end
+--   return nil
+-- end
 M.config = function()
-  local status_ok, gps = pcall(require, "nvim-gps")
-  if not status_ok then
-    gps = {
-      get_location = function()
-        return ""
-      end,
-      is_available = function()
-        return false
-      end,
-    }
-  end
+  -- local status_ok, gps = pcall(require, "nvim-gps")
+  -- if not status_ok then
+  --   gps = {
+  --     get_location = function()
+  --       return ""
+  --     end,
+  --     is_available = function()
+  --       return false
+  --     end,
+  --   }
+  -- end
   local theme = require "user.theme"
 
   local colors
@@ -273,10 +273,10 @@ M.config = function()
     color = { fg = colors.green },
     condition = conditions.hide_in_width,
   }
-  ins_left {
-    gps.get_location,
-    condition = gps_available,
-  }
+  -- ins_left {
+  --   gps.get_location,
+  --   condition = gps_available,
+  -- }
   ins_left {
     lsp_progress,
     condition = conditions.hide_small,
