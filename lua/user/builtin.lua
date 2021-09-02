2df0a50b3cfc3c3900ed523ff2fc928ea927f0ab
abzcoding <abzcoding@gmail.com>
2021-09-03 02:47:35 +0430

    move layout_config to telescope.lua

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -55,32 +55,7 @@ M.config = function()
   -- =========================================
   lvim.builtin.telescope.defaults.path_display = { shorten = 10 }
   lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
-  lvim.builtin.telescope.defaults.layout_config = {
-    width = 0.90,
-    height = 0.85,
-    preview_cutoff = 120,
-    prompt_position = "bottom",
-    horizontal = {
-      preview_width = function(_, cols, _)
-        if cols > 200 then
-          return math.floor(cols * 0.5)
-        else
-          return math.floor(cols * 0.6)
-        end
-      end,
-    },
-    vertical = {
-      width = 0.9,
-      height = 0.95,
-      preview_height = 0.5,
-    },
-
-    flex = {
-      horizontal = {
-        preview_width = 0.9,
-      },
-    },
-  }
+  lvim.builtin.telescope.defaults.layout_config = require("user.telescope").layout_config()
 
   -- Terminal
   -- =========================================
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -4,6 +4,35 @@ local action_state = require "telescope.actions.state"
 local themes = require "telescope.themes"
 local builtin = require "telescope.builtin"
 
+function M.layout_config()
+  return {
+    width = 0.90,
+    height = 0.85,
+    preview_cutoff = 120,
+    prompt_position = "bottom",
+    horizontal = {
+      preview_width = function(_, cols, _)
+        if cols > 200 then
+          return math.floor(cols * 0.5)
+        else
+          return math.floor(cols * 0.6)
+        end
+      end,
+    },
+    vertical = {
+      width = 0.9,
+      height = 0.95,
+      preview_height = 0.5,
+    },
+
+    flex = {
+      horizontal = {
+        preview_width = 0.9,
+      },
+    },
+  }
+end
+
 function M.find_string()
   local opts = {
     border = true,
