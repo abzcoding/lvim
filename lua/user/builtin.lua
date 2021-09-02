34625aa9ae3a62b57dd9305c4e62a6b0dc94eaf4
abzcoding <abzcoding@gmail.com>
2021-09-03 01:01:06 +0430

    wider telescope prompt

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -54,6 +54,33 @@ M.config = function()
   -- Telescope
   -- =========================================
   lvim.builtin.telescope.defaults.path_display = { shorten = 10 }
+  lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
+  lvim.builtin.telescope.defaults.layout_config = {
+    width = 0.95,
+    height = 0.85,
+    preview_cutoff = 120,
+    prompt_position = "bottom",
+    horizontal = {
+      preview_width = function(_, cols, _)
+        if cols > 200 then
+          return math.floor(cols * 0.4)
+        else
+          return math.floor(cols * 0.6)
+        end
+      end,
+    },
+    vertical = {
+      width = 0.9,
+      height = 0.95,
+      preview_height = 0.5,
+    },
+
+    flex = {
+      horizontal = {
+        preview_width = 0.9,
+      },
+    },
+  }
 
   -- Terminal
   -- =========================================
