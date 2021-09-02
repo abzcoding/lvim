87eef16788b5ce3367e5f6e18001578a8a41294b
abzcoding <abzcoding@gmail.com>
2021-09-03 01:46:44 +0430

    slightly smaller telescope preview

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -56,14 +56,14 @@ M.config = function()
   lvim.builtin.telescope.defaults.path_display = { shorten = 10 }
   lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
   lvim.builtin.telescope.defaults.layout_config = {
-    width = 0.95,
+    width = 0.90,
     height = 0.85,
     preview_cutoff = 120,
     prompt_position = "bottom",
     horizontal = {
       preview_width = function(_, cols, _)
         if cols > 200 then
-          return math.floor(cols * 0.4)
+          return math.floor(cols * 0.5)
         else
           return math.floor(cols * 0.6)
         end
