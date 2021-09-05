5c4664e5b75aa2789de05a3f91d42ad9ff50dc8d
abzcoding <abzcoding@gmail.com>
2021-09-05 18:23:43 +0430

    fix lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -288,6 +288,9 @@ M.config = function()
       msg = msg or "LSP Inactive"
       local buf_clients = vim.lsp.buf_get_clients()
       if next(buf_clients) == nil then
+        if #msg == 0 then
+          return ""
+        end
         return msg
       end
       local buf_ft = vim.bo.filetype
