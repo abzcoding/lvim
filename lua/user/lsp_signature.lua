df863cd94bc549bb6f3d79b079d64251657d4a0c
abzcoding <abzcoding@gmail.com>
2021-09-08 16:12:35 +0430

    use_lspsaga is deprecated

--- lua/user/lsp_signature.lua
+++ lua/user/lsp_signature.lua
@@ -9,7 +9,7 @@ M.config = function()
     hint_enable = true, -- virtual hint enable
     hint_prefix = "🐼 ", -- Panda for parameter
     hint_scheme = "String",
-    use_lspsaga = false, -- set to true if you want to use lspsaga popup
+    -- use_lspsaga = false, -- set to true if you want to use lspsaga popup
     hi_parameter = "Search", -- how your parameter will be highlight
     max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
     -- to view the hiding contents
