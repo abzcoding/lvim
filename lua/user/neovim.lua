1964234f5d1b7eba3ba1d78ddac5256f4c9e8218
abzcoding <abzcoding@gmail.com>
2021-09-22 14:14:49 +0330

    better fillchars

--- lua/user/neovim.lua
+++ lua/user/neovim.lua
@@ -9,7 +9,16 @@ M.config = function()
   vim.o.foldlevel = 4
   vim.o.foldtext =
     [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
-  vim.o.fillchars = "fold: "
+  vim.opt.fillchars = {
+    vert = "▕", -- alternatives │
+    fold = " ",
+    eob = " ", -- suppress ~ at EndOfBuffer
+    diff = "╱", -- alternatives = ⣿ ░ ─
+    msgsep = "‾",
+    foldopen = "▾",
+    foldsep = "│",
+    foldclose = "▸",
+  }
   vim.o.foldnestmax = 3
   vim.o.foldminlines = 1
   vim.opt.guifont = "FiraCode Nerd Font:h13"
