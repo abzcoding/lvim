865ed39347a9f387adfbc217542183b3a595cb83
abzcoding <abzcoding@gmail.com>
2021-10-02 16:27:52 +0330

    add markdown tree-sitter parser as well

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -117,13 +117,21 @@ M.config = function()
     lint_events = { "BufWrite", "CursorHold" },
   }
   lvim.builtin.treesitter.on_config_done = function()
-    require("nvim-treesitter.parsers").get_parser_configs().solidity = {
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.solidity = {
       install_info = {
         url = "https://github.com/JoranHonig/tree-sitter-solidity",
         files = { "src/parser.c" },
         requires_generate_from_grammar = true,
       },
       filetype = "solidity",
+    }
+    parser_config.jsonc.used_by = "json"
+    parser_config.markdown = {
+      install_info = {
+        url = "https://github.com/ikatyang/tree-sitter-markdown",
+        files = { "src/parser.c", "src/scanner.cc" },
+      },
     }
   end
 
