398aa32083a6eb7d91c331f1d400ca92870330cb
abzcoding <abzcoding@gmail.com>
2021-10-06 17:01:04 +0330

    better neoclip prompt

--- lua/user/neoclip.lua
+++ lua/user/neoclip.lua
@@ -15,7 +15,26 @@ M.config = function()
     },
   }
   local function clip()
-    require("telescope").extensions.neoclip.default(require("telescope.themes").get_dropdown())
+    local opts = {
+      winblend = 10,
+      layout_strategy = "flex",
+      layout_config = {
+        prompt_position = "top",
+        width = 0.8,
+        height = 0.6,
+        horizontal = { width = { padding = 0.15 } },
+        vertical = { preview_height = 0.70 },
+      },
+      borderchars = {
+        prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+        results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+        preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      },
+      border = {},
+      shorten_path = false,
+    }
+    local dropdown = require("telescope.themes").get_dropdown(opts)
+    require("telescope").extensions.neoclip.default(dropdown)
   end
   local whk_status, whk = pcall(require, "which-key")
   if not whk_status then
