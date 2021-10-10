6591919be07184e220c1a46e5f6090ca96e8244a
abzcoding <abzcoding@gmail.com>
2021-10-10 13:29:57 +0330

    nicer defaults for neovide and nvui

--- lua/user/neovim.lua
+++ lua/user/neovim.lua
@@ -44,6 +44,41 @@ M.config = function()
     -- version control
     ".git,.svn",
   }
+  if vim.g.neovide then
+    vim.g.neovide_cursor_animation_length = 0.01
+    vim.g.neovide_cursor_trail_length = 0.05
+    vim.g.neovide_cursor_antialiasing = true
+    vim.g.neovide_remember_window_size = true
+    vim.cmd [[set guifont=JetBrainsMono\ Nerd\ Font:h16]]
+  end
+
+  if vim.fn.exists "g:nvui" ~= 0 then
+    -- Configure nvui here
+    vim.cmd [[NvuiCmdFontFamily VictorMono Nerd Font]]
+    vim.cmd [[set linespace=1]]
+    vim.cmd [[set guifont=VictorMono\ Nerd\ Font:h18]]
+    vim.cmd [[NvuiPopupMenuDefaultIconFg white]]
+    vim.cmd [[NvuiCmdBg #1e2125]]
+    vim.cmd [[NvuiCmdFg #abb2bf]]
+    vim.cmd [[NvuiCmdBigFontScaleFactor 1.0]]
+    vim.cmd [[NvuiCmdPadding 10]]
+    vim.cmd [[NvuiCmdCenterXPos 0.5]]
+    vim.cmd [[NvuiCmdTopPos 0.0]]
+    vim.cmd [[NvuiCmdFontSize 25.0]]
+    vim.cmd [[NvuiCmdBorderWidth 5]]
+    vim.cmd [[NvuiPopupMenuIconFg variable #56b6c2]]
+    vim.cmd [[NvuiPopupMenuIconFg function #c678dd]]
+    vim.cmd [[NvuiPopupMenuIconFg method #c678dd]]
+    vim.cmd [[NvuiPopupMenuIconFg field #d19a66]]
+    vim.cmd [[NvuiPopupMenuIconFg property #d19a66]]
+    vim.cmd [[NvuiPopupMenuIconFg module white]]
+    vim.cmd [[NvuiPopupMenuIconFg struct #e5c07b]]
+    vim.cmd [[NvuiCaretExtendTop 15]]
+    vim.cmd [[NvuiCaretExtendBottom 8]]
+    vim.cmd [[NvuiTitlebarFontSize 12]]
+    vim.cmd [[NvuiTitlebarFontFamily Arial]]
+    vim.cmd [[NvuiCursorAnimationDuration 0.1]]
+  end
 end
 
 return M
