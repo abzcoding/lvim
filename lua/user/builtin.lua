9499ea21a5df2c94ffcceed39ea850a36c79b75b
abzcoding <abzcoding@gmail.com>
2021-10-22 14:56:53 +0330

    try the new which_key winblend option

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -160,6 +160,8 @@ M.config = function()
 
   -- WhichKey
   -- =========================================
+  lvim.builtin.which_key.setup.window.winblend = 10
+  lvim.builtin.which_key.setup.window.border = "none"
   lvim.builtin.which_key.on_config_done = function(wk)
     local keys = {
       ["ga"] = { "<cmd>lua require('user.telescope').code_actions()<CR>", "Code Action" },
