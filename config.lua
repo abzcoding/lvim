6c3a539a85d333d71e834fbe6a799fe0b5254151
abzcoding <abzcoding@gmail.com>
2021-10-23 11:18:19 +0330

    activate optional notify plugin

--- config.lua
+++ config.lua
@@ -28,6 +28,7 @@ lvim.builtin.latex = {
   preview_exec = "/Applications/Skim.app/Contents/SharedSupport/displayline", -- change this to zathura as well
   rtl_support = true, -- if you want to use xelatex, it's a bit slower but works very well for RTL langs
 }
+lvim.builtin.notify.active = true
 lvim.lsp.automatic_servers_installation = true
 lvim.lsp.document_highlight = true
 lvim.lsp.code_lens_refresh = true
