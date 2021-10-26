7cc269a0b79eae0321b8cc72f7d865b5ed253e1f
abzcoding <abzcoding@gmail.com>
2021-10-26 13:00:13 +0330

    testing the new indent_blankline setup

--- lua/user/indent_blankline.lua
+++ lua/user/indent_blankline.lua
@@ -1,63 +1,71 @@
 local M = {}
 
 M.setup = function()
-  vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-  vim.g.indent_blankline_filetype_exclude = {
-    "alpha",
-    "log",
-    "gitcommit",
-    "vimwiki",
-    "markdown",
-    "json",
-    "txt",
-    "vista",
-    "NvimTree",
-    "git",
-    "TelescopePrompt",
-    "undotree",
-    "flutterToolsOutline",
-    "org",
-    "orgagenda",
-    "help",
-    "startify",
-    "dashboard",
-    "packer",
-    "neogitstatus",
-    "NvimTree",
-    "Trouble",
-    "lspinfo",
-    "", -- for all buffers without a file type
-  }
-  vim.g.indentLine_enabled = 1
-  -- vim.g.indent_blankline_char = "│"
-  vim.g.indent_blankline_char = "▏"
-  vim.g.indent_blankline_show_trailing_blankline_indent = false
-  vim.g.indent_blankline_show_first_indent_level = true
-  vim.g.indent_blankline_use_treesitter = true
-  vim.g.indent_blankline_show_current_context = true
-  vim.g.indent_blankline_context_patterns = {
-    "class",
-    "return",
-    "function",
-    "method",
-    "^if",
-    "^do",
-    "^switch",
-    "^while",
-    "jsx_element",
-    "^for",
-    "^object",
-    "^table",
-    "block",
-    "arguments",
-    "if_statement",
-    "else_clause",
-    "jsx_element",
-    "jsx_self_closing_element",
-    "try_statement",
-    "catch_clause",
-    "import_statement",
-    "operation_type",
+  local status_ok, bl = pcall(require, "indent_blankline")
+  if not status_ok then
+    return
+  end
+  bl.setup {
+    enabled = true,
+    bufname_exclude = { "README.md" },
+    buftype_exclude = { "terminal", "nofile" },
+    filetype_exclude = {
+      "alpha",
+      "log",
+      "gitcommit",
+      "vimwiki",
+      "markdown",
+      "json",
+      "txt",
+      "vista",
+      "NvimTree",
+      "git",
+      "TelescopePrompt",
+      "undotree",
+      "flutterToolsOutline",
+      "org",
+      "orgagenda",
+      "help",
+      "startify",
+      "dashboard",
+      "packer",
+      "neogitstatus",
+      "NvimTree",
+      "Trouble",
+      "lspinfo",
+      "", -- for all buffers without a file type
+    },
+    char = "▏",
+    char_list = { "", "┊", "┆", "¦", "|", "¦", "┆", "┊", "" },
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = false,
+    space_char_blankline = " ",
+    use_treesitter = true,
+    show_current_context = true,
+    context_patterns = {
+      "class",
+      "return",
+      "function",
+      "method",
+      "^if",
+      "^do",
+      "^switch",
+      "^while",
+      "jsx_element",
+      "^for",
+      "^object",
+      "^table",
+      "block",
+      "arguments",
+      "if_statement",
+      "else_clause",
+      "jsx_element",
+      "jsx_self_closing_element",
+      "try_statement",
+      "catch_clause",
+      "import_statement",
+      "operation_type",
+    },
   }
   -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
   vim.wo.colorcolumn = "99999"
