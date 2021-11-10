5af75cab8470af9524451e7de5ede1db3f439d9b
abzcoding <abzcoding@gmail.com>
2021-11-10 13:47:08 +0330

    better commenting for ts and tsx files

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -62,6 +62,30 @@ M.config = function()
     end
   end
 
+  -- Comment
+  -- =========================================
+  -- integrate with nvim-ts-context-commentstring
+  lvim.builtin.comment.pre_hook = function(ctx)
+    if not vim.tbl_contains({ "typescript", "typescriptreact" }, vim.bo.ft) then
+      return
+    end
+
+    local comment_utils = require "Comment.utils"
+    local type = ctx.ctype == comment_utils.ctype.line and "__default" or "__multiline"
+
+    local location
+    if ctx.ctype == comment_utils.ctype.block then
+      location = require("ts_context_commentstring.utils").get_cursor_location()
+    elseif ctx.cmotion == comment_utils.cmotion.v or ctx.cmotion == comment_utils.cmotion.V then
+      location = require("ts_context_commentstring.utils").get_visual_start_location()
+    end
+
+    return require("ts_context_commentstring.internal").calculate_commentstring {
+      key = type,
+      location = location,
+    }
+  end
+
   -- Dashboard
   -- =========================================
   lvim.builtin.dashboard.active = not lvim.builtin.fancy_dashboard.active
