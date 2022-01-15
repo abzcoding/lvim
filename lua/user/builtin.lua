b6979b189f5c896d934d56dbf44591780df7c49f
abzcoding <abzcoding@gmail.com>
2022-01-15 16:40:52 +0330

    cmdline bug was caused by nvim-cmp, which is fixed now

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -61,13 +61,6 @@ M.config = function()
   if lvim.builtin.sell_your_soul_to_devil then
     lvim.keys.insert_mode["<c-h>"] = { [[copilot#Accept("\<CR>")]], { expr = true, script = true } }
     local cmp = require "cmp"
-    cmp.setup.cmdline(":", {
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
-    })
     lvim.builtin.cmp.mapping["<Tab>"] = cmp.mapping(M.tab, { "i", "c" })
     lvim.builtin.cmp.mapping["<S-Tab>"] = cmp.mapping(M.shift_tab, { "i", "c" })
   end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -576,9 +576,6 @@ M.config = function()
       requires = { "nvim-lua/plenary.nvim" },
       disable = not lvim.builtin.metals.active,
     },
-    {
-      "hrsh7th/cmp-cmdline",
-    },
   }
 end
 
