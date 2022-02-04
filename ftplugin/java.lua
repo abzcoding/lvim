d50db1ea085c02d140887cf9a72ea05c29fcb20e
abzcoding <abzcoding@gmail.com>
2022-02-04 12:03:39 +0330

    better java formatting

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -7,14 +7,17 @@ local workspace_path = os.getenv "HOME" .. "/workspace/"
 local JAVA_LS_EXECUTABLE = os.getenv "HOME" .. "/.local/share/lunarvim/lvim/utils/bin/jdtls"
 
 jdtls.start_or_attach {
-  on_attach = require("lvim.lsp").common_on_attach,
+  on_attach = function()
+    require("jdtls.setup").add_commands()
+    require("lvim.lsp").common_on_attach()
+  end,
+  flags = {
+    allow_incremental_sync = true,
+    server_side_fuzzy_completion = true,
+  },
+  settings = {
+    ["java.format.settings.url"] = vim.fn.expand "~/" .. ".config/lvim/.java-google-formatter.xml",
+    ["java.format.settings.profile"] = "GoogleStyle",
+  },
   cmd = { JAVA_LS_EXECUTABLE, workspace_path .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t") },
 }
-
-vim.api.nvim_set_keymap("n", "<leader>la", ":lua require('jdtls').code_action()<CR>", { noremap = true, silent = true })
-
-vim.cmd "command! -buffer JdtCompile lua require('jdtls').compile()"
-vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"
--- vim.cmd "command! -buffer JdtJol lua require('jdtls').jol()"
-vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
--- vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
