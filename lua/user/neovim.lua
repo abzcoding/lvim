d5bda9cfe86226f8f750a16012f8c77d71369157
abzcoding <abzcoding@gmail.com>
2022-03-27 13:16:36 +0430

    a bit cleaner quickfix ui, also fix some bugs in asynctasks

--- lua/user/neovim.lua
+++ lua/user/neovim.lua
@@ -118,6 +118,7 @@ M.config = function()
     precedes = "‹", -- Alternatives: … «
     trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
   }
+  vim.o.qftf = '{info -> v:lua._G.qftf(info)}'
 
   -- Cursorline highlighting control
   --  Only have it on in the active buffer
@@ -195,6 +196,48 @@ function M.maximize_current_split()
     vim.cmd "tabedit %:p"
     vim.api.nvim_win_set_cursor(0, last_cursor)
   end
+end
+
+function _G.qftf(info)
+  local fn = vim.fn
+  local items
+  local ret = {}
+  if info.quickfix == 1 then
+    items = fn.getqflist({ id = info.id, items = 0 }).items
+  else
+    items = fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+  end
+  local limit = 25
+  local fname_fmt1, fname_fmt2 = "%-" .. limit .. "s", "…%." .. (limit - 1) .. "s"
+  local valid_fmt, invalid_fmt = "%s |%5d:%-3d|%s %s", "%s"
+  for i = info.start_idx, info.end_idx do
+    local e = items[i]
+    local fname = ""
+    local str
+    if e.valid == 1 then
+      if e.bufnr > 0 then
+        fname = vim.api.nvim_buf_get_name(e.bufnr)
+        if fname == "" then
+          fname = "[No Name]"
+        else
+          fname = fname:gsub("^" .. vim.env.HOME, "~")
+        end
+        if fn.strwidth(fname) <= limit then
+          fname = fname_fmt1:format(fname)
+        else
+          fname = fname_fmt2:format(fname:sub(1 - limit, -1))
+        end
+      end
+      local lnum = e.lnum > 99999 and "inf" or e.lnum
+      local col = e.col > 999 and "inf" or e.col
+      local qtype = e.type == "" and "" or " " .. e.type:sub(1, 1):upper()
+      str = valid_fmt:format(fname, lnum, col, qtype, e.text)
+    else
+      str = invalid_fmt:format(e.text)
+    end
+    table.insert(ret, str)
+  end
+  return ret
 end
 
 return M
