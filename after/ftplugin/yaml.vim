f236e40d2939d0fcc257c7f4f586c9cfdf21f365
Soren Martius <soeren.martius@gmail.com>
2022-04-08 09:57:38 +0200

    chore: add ftplugin files for yaml and vue

---
+++ after/ftplugin/yaml.vim
@@ -0,0 +1,6 @@
+setlocal tabstop=4
+setlocal shiftwidth=2
+setlocal autoindent
+setlocal smarttab
+setlocal expandtab
+
--- lua/user/neovim.lua
+++ lua/user/neovim.lua
@@ -25,9 +25,9 @@ M.config = function()
     vim.g["loaded_" .. plugin] = 1
   end
   -- vim.g.did_load_filetypes = 1
-  -- vim.opt.expandtab = true -- convert tabs to spaces
-  -- vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
-  -- vim.opt.fileencoding = "utf-8" -- the encoding written to a file
+  vim.opt.expandtab = true -- convert tabs to spaces
+  vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+  vim.opt.fileencoding = "utf-8" -- the encoding written to a file
   vim.opt.ignorecase = true -- ignore case in search patterns
   vim.opt.smartcase = true -- smart case
   -- vim.opt.smartindent = true -- make indenting smarter again
