a46d747bf664eff161adfd3bbb8d3675623f4fde
abzcoding <abzcoding@gmail.com>
2022-05-06 11:23:33 +0430

    fix #42

--- lua/user/tss.lua
+++ lua/user/tss.lua
@@ -3,11 +3,21 @@ local M = {}
 local api = vim.api
 
 local get_map_options = function(custom_options)
-    local options = { silent = true }
-    if custom_options then
-        options = vim.tbl_extend("force", options, custom_options)
-    end
-    return options
+  local options = { silent = true }
+  if custom_options then
+    options = vim.tbl_extend("force", options, custom_options)
+  end
+  return options
+end
+
+M.map = function(mode, target, source, opts)
+  vim.keymap.set(mode, target, source, get_map_options(opts))
+end
+
+for _, mode in ipairs({ "n", "o", "i", "x", "t", "c" }) do
+  M[mode .. "map"] = function(...)
+    M.map(mode, ...)
+  end
 end
 
 M.buf_map = function(bufnr, mode, target, source, opts)
@@ -18,105 +28,105 @@ M.buf_map = function(bufnr, mode, target, source, opts)
 end
 
 M.input = function(keys, mode)
-    api.nvim_feedkeys(M.t(keys), mode or "m", true)
+  api.nvim_feedkeys(M.t(keys), mode or "m", true)
 end
 
 M.warn = function(msg)
-    api.nvim_echo({ { msg, "WarningMsg" } }, true, {})
+  api.nvim_echo({ { msg, "WarningMsg" } }, true, {})
 end
 
 M.gfind = function(str, substr, cb, init)
-    init = init or 1
-    local start_pos, end_pos = str:find(substr, init)
-    if start_pos then
-        cb(start_pos, end_pos)
-        return M.gfind(str, substr, cb, end_pos + 1)
-    end
+  init = init or 1
+  local start_pos, end_pos = str:find(substr, init)
+  if start_pos then
+    cb(start_pos, end_pos)
+    return M.gfind(str, substr, cb, end_pos + 1)
+  end
 end
 
 -- const myString = "hello ${}" ->
 -- const myString = `hello ${}`
 local change_template_string_quotes = function()
-    local row, col = unpack(api.nvim_win_get_cursor(0))
-    row = row - 1
+  local row, col = unpack(api.nvim_win_get_cursor(0))
+  row = row - 1
 
-    local quote_start, quote_end
-    M.gfind(api.nvim_get_current_line(), "[\"']", function(pos)
-        if not quote_start then
-            -- start at first quote
-            quote_start = pos
-        elseif pos < col then
-            -- move start if quote is closer to col
-            if (pos - col) > (quote_start - col) then
-                quote_start = pos
-            end
-        elseif not quote_end then
-            -- first quote after col is end
-            quote_end = pos
-        end
-    end)
-
-    -- if found, replace quotes with backticks
-    if quote_start and quote_start <= col and quote_end then
-        api.nvim_buf_set_text(0, row, quote_start - 1, row, quote_start, { "`" })
-        api.nvim_buf_set_text(0, row, quote_end - 1, row, quote_end, { "`" })
+  local quote_start, quote_end
+  M.gfind(api.nvim_get_current_line(), "[\"']", function(pos)
+    if not quote_start then
+      -- start at first quote
+      quote_start = pos
+    elseif pos < col then
+      -- move start if quote is closer to col
+      if (pos - col) > (quote_start - col) then
+        quote_start = pos
+      end
+    elseif not quote_end then
+      -- first quote after col is end
+      quote_end = pos
     end
+  end)
 
-    -- input and move cursor into pair
-    M.input("${}", "n")
-    M.input("<Left>")
+  -- if found, replace quotes with backticks
+  if quote_start and quote_start <= col and quote_end then
+    api.nvim_buf_set_text(0, row, quote_start - 1, row, quote_start, { "`" })
+    api.nvim_buf_set_text(0, row, quote_end - 1, row, quote_end, { "`" })
+  end
+
+  -- input and move cursor into pair
+  M.input("${}", "n")
+  M.input("<Left>")
 end
 
 -- padding: 40px; ->
 -- padding: "40px",
 local css_to_js = function(opts)
-    local start_line, end_line
-    if type(opts) == "table" then
-        -- called via command
-        start_line, end_line = opts.line1 - 1, opts.line2
-    else
-        -- called as operator
-        start_line = api.nvim_buf_get_mark(0, "[")[1] - 1
-        end_line = api.nvim_buf_get_mark(0, "]")[1] + 1
+  local start_line, end_line
+  if type(opts) == "table" then
+    -- called via command
+    start_line, end_line = opts.line1 - 1, opts.line2
+  else
+    -- called as operator
+    start_line = api.nvim_buf_get_mark(0, "[")[1] - 1
+    end_line = api.nvim_buf_get_mark(0, "]")[1] + 1
+  end
+
+  local did_convert = false
+  for i, line in ipairs(api.nvim_buf_get_lines(0, start_line, end_line, false)) do
+    -- if the line ends in a comma, it's probably already js
+    if line:sub(#line) == "," then
+      goto continue
+    end
+    -- ignore comments
+    if line:find("%/%*") then
+      goto continue
     end
 
-    local did_convert = false
-    for i, line in ipairs(api.nvim_buf_get_lines(0, start_line, end_line, false)) do
-        -- if the line ends in a comma, it's probably already js
-        if line:sub(#line) == "," then
-            goto continue
-        end
-        -- ignore comments
-        if line:find("%/%*") then
-            goto continue
-        end
-
-        local indentation, name, val = line:match("(%s+)(.+):%s(.+)")
-        -- skip non-matching lines
-        if not (name and val) then
-            goto continue
-        end
-
-        local parsed_name = ""
-        for j, component in ipairs(vim.split(name, "-")) do
-            parsed_name = parsed_name .. (j == 1 and component or (component:sub(1, 1):upper() .. component:sub(2)))
-        end
-
-        local parsed_val = val:gsub(";", "")
-        -- keep numbers, wrap others in quotes
-        parsed_val = tonumber(parsed_val) or string.format('"%s"', parsed_val)
-        local parsed_line = table.concat({ indentation, parsed_name, ": ", parsed_val, "," })
-
-        did_convert = true
-        local row = start_line + i
-        api.nvim_buf_set_lines(0, row - 1, row, false, { parsed_line })
-
-        ::continue::
+    local indentation, name, val = line:match("(%s+)(.+):%s(.+)")
+    -- skip non-matching lines
+    if not (name and val) then
+      goto continue
     end
 
-    if not did_convert then
-        M.warn("css-to-js: nothing to convert")
+    local parsed_name = ""
+    for j, component in ipairs(vim.split(name, "-")) do
+      parsed_name = parsed_name .. (j == 1 and component or (component:sub(1, 1):upper() .. component:sub(2)))
     end
+
+    local parsed_val = val:gsub(";", "")
+    -- keep numbers, wrap others in quotes
+    parsed_val = tonumber(parsed_val) or string.format('"%s"', parsed_val)
+    local parsed_line = table.concat({ indentation, parsed_name, ": ", parsed_val, "," })
+
+    did_convert = true
+    local row = start_line + i
+    api.nvim_buf_set_lines(0, row - 1, row, false, { parsed_line })
+
+    ::continue::
+  end
+
+  if not did_convert then
+    M.warn("css-to-js: nothing to convert")
+  end
 end
 
 _G.css_to_js = css_to_js
