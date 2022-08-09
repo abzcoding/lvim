3f20ec1141795d1706f22594369e1c2e4d84be79
abzcoding <abzcoding@gmail.com>
2022-08-09 11:32:36 +0430

    fix hop for `t` and `T`

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -51,13 +51,13 @@ M.set_hop_keymaps = function()
   vim.api.nvim_set_keymap(
     "",
     "t",
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
     {}
   )
   vim.api.nvim_set_keymap(
     "",
     "T",
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
     {}
   )
 end
