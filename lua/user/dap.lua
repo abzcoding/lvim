d593ca86fdfc47869e9dcc834b1cf9453335ebed
abzcoding <abzcoding@gmail.com>
2022-09-07 11:15:42 +0430

    extra dap config for rust

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -34,6 +34,31 @@ M.config = function()
     },
   }
 
+  -- NOTE: if you want to use `dap` instead of `RustDebuggables` you can use the following configuration
+  if vim.fn.executable "lldb-vscode" == 1 then
+    dap.adapters.lldbrust = {
+      type = "executable",
+      attach = { pidProperty = "pid", pidSelect = "ask" },
+      command = "lldb-vscode",
+      env = { LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES" },
+    }
+    dap.adapters.rust = dap.adapters.lldbrust
+    dap.configurations.rust = {
+      {
+        type = "rust",
+        request = "launch",
+        name = "lldbrust",
+        program = function()
+          local metadata_json = vim.fn.system "cargo metadata --format-version 1 --no-deps"
+          local metadata = vim.fn.json_decode(metadata_json)
+          local target_name = metadata.packages[1].targets[1].name
+          local target_dir = metadata.target_directory
+          return target_dir .. "/debug/" .. target_name
+        end,
+      },
+    }
+  end
+
   dap.adapters.go = function(callback, _)
     local stdout = vim.loop.new_pipe(false)
     local handle
