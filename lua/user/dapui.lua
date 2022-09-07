a285b0210af46158f868f087d7ca4eed80c52b07
abzcoding <abzcoding@gmail.com>
2022-09-07 11:11:46 +0430

    cleaner dap-ui

--- lua/user/dapui.lua
+++ lua/user/dapui.lua
@@ -7,6 +7,7 @@ M.config = function()
   end
 
   ui.setup {
+    expand_lines = true,
     icons = {
       expanded = "",
       collapsed = "",
@@ -14,16 +15,22 @@ M.config = function()
     },
     layouts = {
       {
-        elements = { "scopes", "watches" },
-        size = 40,
+        elements = {
+          { id = "scopes", size = 0.33 },
+          { id = "breakpoints", size = 0.17 },
+          { id = "stacks", size = 0.25 },
+          { id = "watches", size = 0.25 },
+        },
+        size = 0.31,
         position = "left",
       },
       {
-        elements = { "repl", "console" },
-        size = 10,
+        elements = { { id = "repl", size = 0.45 }, { id = "console", size = 0.55 } },
+        size = 0.26,
         position = "bottom",
       },
     },
+    floating = { max_width = 0.9, max_height = 0.5, border = vim.g.border_chars },
   }
 end
 
