0bc4fde98cf5f6f966b60ad4946936b7bc7d4b1a
abzcoding <abzcoding@gmail.com>
2022-09-09 18:09:06 +0430

    warn users about installing codelldb

--- lua/user/rust_tools.lua
+++ lua/user/rust_tools.lua
@@ -41,21 +41,48 @@ M.config = function()
       on_init = require("lvim.lsp").common_on_init,
     },
   }
-  local path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/")
-    or vim.fn.expand "~/" .. ".vscode/extensions/vadimcn.vscode-lldb-1.7.3/"
-  local codelldb_path = path .. "adapter/codelldb"
-  local liblldb_path = path .. "lldb/lib/liblldb.so"
+  local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/")
+  local vscode_path = vim.fn.expand "~/" .. ".vscode/extensions/vadimcn.vscode-lldb-1.7.4/"
 
-  if vim.fn.has "mac" == 1 then
-    liblldb_path = path .. "lldb/lib/liblldb.dylib"
+  local path = ""
+  local debugger_found = true
+  if M.dir_exists(mason_path) then
+    path = mason_path
+  elseif M.dir_exists(vscode_path) then
+    path = vscode_path
+  else
+    debugger_found = false
+    vim.notify("please install codelldb using :Mason or via vscode", vim.log.levels.WARN)
   end
 
-  if vim.fn.filereadable(codelldb_path) and vim.fn.filereadable(liblldb_path) then
-    opts.dap = {
-      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-    }
+  if debugger_found then
+    local codelldb_path = path .. "adapter/codelldb"
+    local liblldb_path = path .. "lldb/lib/liblldb.so"
+
+    if vim.fn.has "mac" == 1 then
+      liblldb_path = path .. "lldb/lib/liblldb.dylib"
+    end
+
+    if vim.fn.filereadable(codelldb_path) and vim.fn.filereadable(liblldb_path) then
+      opts.dap = {
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+      }
+    else
+      vim.notify("please reinstall codellb, i cannot find liblldb or codelldb itself", vim.log.levels.WARN)
+    end
   end
   rust_tools.setup(opts)
+end
+
+M.dir_exists = function(file)
+  local ok, _, code = os.rename(file, file)
+  if not ok then
+    if code == 13 then
+      -- Permission denied, but it exists
+      return true
+    end
+  end
+  return ok
 end
 
 return M
