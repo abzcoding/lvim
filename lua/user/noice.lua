9288423287650885243679bf7b75dd41e201b1d5
abzcoding <abzcoding@gmail.com>
2022-10-04 11:33:26 +0330

    cleanup noice

--- lua/user/noice.lua
+++ lua/user/noice.lua
@@ -12,6 +12,36 @@ M.config = function()
     popupmenu = {
       enabled = not lvim.builtin.fancy_wild_menu.active,
     },
+    routes = {
+      {
+        filter = { event = "msg_show", kind = "search_count" },
+        opts = { skip = true },
+      },
+      {
+        view = "split",
+        filter = { event = "msg_show", min_height = 20 },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "yanked",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = { find = "No active Snippet" },
+        opts = { skip = true },
+      },
+    },
   }
 end
 
