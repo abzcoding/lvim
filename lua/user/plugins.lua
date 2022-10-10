f87221bfd3b74243ab65dca731724cded0770534
abzcoding <abzcoding@gmail.com>
2022-10-10 23:47:52 +0330

    use the new sqlite.nvim repo

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.config = function()
-  local neoclip_req = { "tami5/sqlite.lua", module = "sqlite" }
+  local neoclip_req = { "kkharji/sqlite.lua", module = "sqlite" }
   if lvim.builtin.neoclip.enable_persistent_history == false then
     neoclip_req = {}
   end
