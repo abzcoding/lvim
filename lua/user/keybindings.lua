d52d9593390a0b2b6fdbdd876b176839fe92f565
Uzair Aftab <uzaaft@outlook.com>
2022-12-29 17:29:55 +0100

    fix(keybindings): Fix keymaps to enable lazyloading Neotree

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -276,7 +276,7 @@ M.config = function()
     M.set_lsp_lines_keymap()
   end
   if lvim.builtin.tree_provider == "neo-tree" then
-    lvim.builtin.which_key.mappings["e"] = { ":NeoTreeRevealToggle<CR>", " Explorer" }
+    lvim.builtin.which_key.mappings["e"] = { "<cmd>Neotree toggle<CR>", " Explorer" }
   end
   lvim.builtin.which_key.mappings["F"] = {
     name = " Find",
