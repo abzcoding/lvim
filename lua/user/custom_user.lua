f78cd456394ff5816f80753ce5ea7f1489b9a37e
abzcoding <abzcoding@gmail.com>
2023-03-19 20:42:14 +0330

    using tmux_lualine again

--- lua/user/custom_user.lua
+++ lua/user/custom_user.lua
@@ -10,13 +10,15 @@ M.config = function()
 
   lvim.builtin.lsp_lines = true
   vim.diagnostic.config { virtual_lines = false } -- i only want to use it explicitly ( by calling the toggle function)
-  lvim.builtin.tmux_lualine = false
+  lvim.builtin.tmux_lualine = true
   if lvim.builtin.tmux_lualine then
     vim.opt.cmdheight = 0
     vim.opt.laststatus = 0
     vim.g.tpipeline_cursormoved = 1
     -- HACK: lualine hijacks the statusline, so we need to set it back to what we want
-    vim.cmd [[ autocmd WinEnter,BufEnter,VimResized * setlocal laststatus=0 ]]
+    if vim.env.TMUX then
+      vim.cmd [[ autocmd WinEnter,BufEnter,VimResized * setlocal laststatus=0 ]]
+    end
   end
   lvim.builtin.custom_web_devicons = true
   lvim.use_icons = false -- only set to false if you know what are you doing
