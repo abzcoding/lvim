a587d753692f479f8ade3c59a17fab19e3456ff3
abzcoding <abzcoding@gmail.com>
2023-03-28 17:36:47 +0330

    add deprecation notice for mind.nvim

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -483,9 +483,11 @@ M.telescope_theme = function(colorset)
     link("@lsp.typemod.function.defaultLibrary", "Special")
     link("@lsp.typemod.variable.defaultLibrary", "@variable.builtin")
     link("@lsp.typemod.variable.global", "@constant.builtin")
-    link("@lsp.typemod.operator.injected", "@operator")
-    link("@lsp.typemod.string.injected", "@string")
-    link("@lsp.typemod.variable.injected", "@variable")
+    link("@lsp.typemod.operator", "@operator")
+    link("@lsp.typemod.string", "@string")
+    link("@lsp.typemod.variable", "@variable")
+    link("@lsp.typemod.parameter.label", "@field")
+    link("@type.qualifier", "@keyword")
   end
 
   -- NOTE: these are my personal preferences
