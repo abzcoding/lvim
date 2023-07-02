665b4d7705f371f8c2d04024b24903951a00e1ad
abzcoding <abzcoding@gmail.com>
2023-07-02 18:46:57 +0330

    remove offending overseer template
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/user/ovs.lua
+++ lua/user/ovs.lua
@@ -50,30 +50,6 @@ M.config = function()
     },
   }
 
-  see.register_template {
-    generator = function()
-      local logHandler = io.popen [[fd -e log]]
-      local ret = {}
-      if logHandler then
-        local logs = logHandler:read "*a"
-        logHandler:close()
-        for log in logs:gmatch "([^\r\n]+)" do
-          table.insert(ret, {
-            name = "Show " .. log,
-            builder = function()
-              return {
-                name = "Show " .. log,
-                cmd = "tail --follow --retry " .. log,
-              }
-            end,
-            priority = 1000,
-            params = {},
-          })
-        end
-      end
-      return ret
-    end,
-  }
 end
 
 return M
