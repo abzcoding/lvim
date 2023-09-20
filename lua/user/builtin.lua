a4e400f0ffaba68377cca432566e54617dfeb2ca
abzcoding <abzcoding@gmail.com>
2023-09-20 14:25:23 +0330

    enhanced float handler thanks to @MariaSolOs
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -316,22 +316,8 @@ M.config = function()
       vim.lsp.handlers["textDocument/hover"] = noice_util.protect(require("noice.lsp.hover").on_hover)
     end
   else
-    local float_config = {
-      focusable = true,
-      style = "minimal",
-      border = {
-        { "╔", "FloatBorder" },
-        { "═", "FloatBorder" },
-        { "╗", "FloatBorder" },
-        { "║", "FloatBorder" },
-        { "╝", "FloatBorder" },
-        { "═", "FloatBorder" },
-        { "╚", "FloatBorder" },
-        { "║", "FloatBorder" },
-      },
-    }
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float_config)
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float_config)
+    vim.lsp.handlers["textDocument/hover"] = M.enhanced_float_handler(vim.lsp.handlers.hover)
+    vim.lsp.handlers["textDocument/signatureHelp"] = M.enhanced_float_handler(vim.lsp.handlers.signature_help)
   end
 
   -- NvimTree
@@ -907,6 +893,77 @@ M.lsp_on_attach_callback = function(client, _)
     end
   end
   which_key.register(mappings, opts)
+end
+
+M.enhanced_float_handler = function(handler)
+  return function(err, result, ctx, config)
+    local buf, win = handler(
+      err,
+      result,
+      ctx,
+      vim.tbl_deep_extend("force", config or {}, {
+        border = "rounded",
+        max_height = math.floor(vim.o.lines * 0.5),
+        max_width = math.floor(vim.o.columns * 0.4),
+      })
+    )
+
+    if not buf or not win then
+      return
+    end
+
+    -- Conceal everything.
+    vim.wo[win].concealcursor = "n"
+
+    -- Extra highlights.
+    for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+      for pattern, hl_group in pairs {
+        ["|%S-|"] = "@text.reference",
+        ["@%S+"] = "@parameter",
+        ["^%s*(Parameters:)"] = "@text.title",
+        ["^%s*(Return:)"] = "@text.title",
+        ["^%s*(See also:)"] = "@text.title",
+        ["{%S-}"] = "@parameter",
+      } do
+        local from = 1 ---@type integer?
+        while from do
+          local to
+          from, to = line:find(pattern, from)
+          if from then
+            vim.api.nvim_buf_set_extmark(buf, md_namespace, l - 1, from - 1, {
+              end_col = to,
+              hl_group = hl_group,
+            })
+          end
+          from = to and to + 1 or nil
+        end
+      end
+    end
+
+    -- Add keymaps for opening links.
+    if not vim.b[buf].markdown_keys then
+      vim.keymap.set("n", "K", function()
+        -- Vim help links.
+        local url = (vim.fn.expand "<cWORD>" --[[@as string]]):match "|(%S-)|"
+        if url then
+          return vim.cmd.help(url)
+        end
+
+        -- Markdown links.
+        local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+        local from, to
+        from, to, url = vim.api.nvim_get_current_line():find "%[.-%]%((%S-)%)"
+        if from and col >= from and col <= to then
+          vim.system({ "open", url }, nil, function(res)
+            if res.code ~= 0 then
+              vim.notify("Failed to open URL" .. url, vim.log.levels.ERROR)
+            end
+          end)
+        end
+      end, { buffer = buf, silent = true })
+      vim.b[buf].markdown_keys = true
+    end
+  end
 end
 
 return M
