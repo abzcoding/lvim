6336774b7127b60fca45ef61d31786647a5f9300
abzcoding <abzcoding@gmail.com>
2023-09-27 20:44:20 +0330

    fancy folds
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/user/neovim.lua
+++ lua/user/neovim.lua
@@ -51,8 +51,6 @@ M.config = function()
   vim.wo.foldmethod = "expr"
   vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
   vim.wo.foldlevel = 4
-  vim.wo.foldtext =
-    [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
   vim.wo.foldnestmax = 3
   vim.wo.foldminlines = 1
   vim.opt.guifont = "FiraCode Nerd Font:h13"
--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -1,5 +1,44 @@
 local M = {}
 
+function HighlightedFoldtext()
+  local pos = vim.v.foldstart
+  local line = vim.api.nvim_buf_get_lines(0, pos - 1, pos, false)[1]
+  local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+  local parser = vim.treesitter.get_parser(0, lang)
+  local query = vim.treesitter.query.get(parser:lang(), "highlights")
+
+  if query == nil then
+    return vim.fn.foldtext()
+  end
+
+  local tree = parser:parse({ pos - 1, pos })[1]
+  local result = {}
+  local line_pos = 0
+  local prev_range = nil
+
+  for id, node, _ in query:iter_captures(tree:root(), 0, pos - 1, pos) do
+    local name = query.captures[id]
+    local start_row, start_col, end_row, end_col = node:range()
+    if start_row == pos - 1 and end_row == pos - 1 then
+      local range = { start_col, end_col }
+      if start_col > line_pos then
+        table.insert(result, { line:sub(line_pos + 1, start_col), "Folded" })
+      end
+      line_pos = end_col
+      local text = vim.treesitter.get_node_text(node, 0)
+      if prev_range ~= nil and range[1] == prev_range[1] and range[2] == prev_range[2] then
+        result[#result] = { text, "@" .. name }
+      else
+        table.insert(result, { text, "@" .. name })
+      end
+      prev_range = range
+    end
+  end
+  result[#result] = {" {...} ( ".. tostring(vim.v.foldend - vim.v.foldstart) .. " lines)", "Folded"}
+
+  return result
+end
+
 M.tokyonight = function()
   -- require("tokyonight").setup {
   lvim.builtin.theme.tokyonight.options = {
@@ -540,6 +579,12 @@ M.telescope_theme = function(colorset)
     set_fg_bg("TelescopeResultsBorder", colors.bg, colors.bg)
     set_bg("TelescopeSelection", colors.bg_alt)
   end
+
+  local bg = vim.api.nvim_get_hl(0, { name = "StatusLine" }).bg
+  local hl = vim.api.nvim_get_hl(0, { name = "Folded" })
+  hl.bg = bg
+  vim.api.nvim_set_hl(0, "Folded", hl)
+  vim.opt.foldtext = [[luaeval('HighlightedFoldtext')()]]
 
   if not lvim.builtin.telescope.active then
     set_fg_bg("FzfLuaBorder", colors.bg, colors.bg)
